bd9b61cb85371b1aef55ce900a06e50f50536d9b
Harshul Jain <harshuljain.mait.it@gmail.com>
2021-07-10 12:39:41 +0530

    Completed

--- app/src/main/java/com/example/whatsappopener/MainActivity.kt
+++ app/src/main/java/com/example/whatsappopener/MainActivity.kt
@@ -11,34 +11,33 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        var number:String=" "
-        if(intent.action== Intent.ACTION_PROCESS_TEXT){
-            number=intent.getCharSequenceExtra(Intent.EXTRA_PROCESS_TEXT).toString()
+        var number: String = " "
+        if (intent.action == Intent.ACTION_PROCESS_TEXT) {
+            number = intent.getCharSequenceExtra(Intent.EXTRA_PROCESS_TEXT).toString()
         }
-        if(number.isDigitsOnly()){
+        if (number.isDigitsOnly()) {
             startWhatsapp(number)
-        }
-        else{
-            Toast.makeText(this,"Please check the number",Toast.LENGTH_SHORT).show()
+        } else {
+            Toast.makeText(this, "Please check the number", Toast.LENGTH_SHORT).show()
         }
 
     }
 
     private fun startWhatsapp(number: String) {
-        val intent=Intent(Intent.ACTION_VIEW)
+        val intent = Intent(Intent.ACTION_VIEW)
         intent.setPackage("com.whatsapp")
-        val data:String=if(number[0]=='+'){
+        val data: String = if (number[0] == '+') {
             number.substring(1)
-        }else if(number.length==10){
-            "91"+number
-        }else{
+        } else if (number.length == 10) {
+            "91" + number
+        } else {
             number
         }
-        intent.data=Uri.parse("https://wa.me/$data")
-        if(packageManager.resolveActivity(intent,0)!=null){
+        intent.data = Uri.parse("https://wa.me/$data")
+        if (packageManager.resolveActivity(intent, 0) != null) {
             startActivity(intent)
-        }else{
-            Toast.makeText(this,"Please install Whatsapp",Toast.LENGTH_SHORT).show()
+        } else {
+            Toast.makeText(this, "Please install Whatsapp", Toast.LENGTH_SHORT).show()
         }
         finish()
     }
